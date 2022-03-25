d0ecf8d60a0fb664868b47006a9b90ea6fda844e
princek18 <prince.kumar@hexaviewtech.com>
2022-03-25 14:14:56 +0530

    Server url

--- src/Components/MainComponents/MainComponent.jsx
+++ src/Components/MainComponents/MainComponent.jsx
@@ -4,8 +4,8 @@ import { InputComponent } from './Input/InputComponent'
 import { Todos } from './Todos/Todos'
 import { Loader } from '../Loader/Loader';
 
-export const baseUrl = "http://localhost:4000";
-// export const baseUrl = "https://todo-server-pk18.herokuapp.com";
+// export const baseUrl = "http://localhost:4000";
+export const baseUrl = "https://todo-server-pk18.herokuapp.com";
 
 export const MainComponent = () => {
   const [flag, setFlag] = useState(false);
