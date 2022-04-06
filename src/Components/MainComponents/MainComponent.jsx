b9a0139f01aab89f3f8cdc1901e35744df57b79c
princek18 <prince.kumar@hexaviewtech.com>
2022-04-06 11:53:14 +0530

    Base url change

--- src/Components/MainComponents/MainComponent.jsx
+++ src/Components/MainComponents/MainComponent.jsx
@@ -3,8 +3,8 @@ import './MainComponent.css'
 import { InputComponent } from './Input/InputComponent'
 import { Todos } from './Todos/Todos'
 
-export const baseUrl = "http://localhost:4000";
-// export const baseUrl = "https://todo-server-pk18.herokuapp.com";
+// export const baseUrl = "http://localhost:4000";
+export const baseUrl = "https://todo-server-pk18.herokuapp.com";
 
 export const MainComponent = () => {
   const [flag, setFlag] = useState(false);
